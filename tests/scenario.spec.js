8e030dcdca4e63b4f6bc6be65ebb44b1e044ba36
kutralakumaran K <kumarancool20@yahoo.com>
2024-01-30 07:19:10 +0000

    update

--- tests/scenario.spec.js
+++ tests/scenario.spec.js
@@ -1,6 +1,25 @@
 const { test, expect, chromium, firefox } = require('@playwright/test')
 
-test("first Scenario test", async ({page}) => {
+test("first Scenario test", async () => {
+    const capabilities = {
+        'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'Windows 10',
+            'build': 'Playwright Sample Build',
+            'name': 'Playwright Sample Test',
+            'user': 'kumarancool20',
+            'accessKey': 'r0hT9zVdrW2RblbW2hUvS5YaBZoGctGymCFyR7JG0KrWfS6Jhq',
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+    const page = await browser.newPage()
     await page.goto("https://www.lambdatest.com/selenium-playground/");
     await page.locator('//a[text()="Simple Form Demo"]').click();
     let urlContains = await page.url();
@@ -11,7 +30,26 @@ test("first Scenario test", async ({page}) => {
     await expect(text).toContain('Welcome to LambdaTest')
 
 });
-test("Second Scenario test", async ({page}) => {
+test("Second Scenario test", async () => {
+    const capabilities = {
+        'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'Windows 10',
+            'build': 'Playwright Sample Build',
+            'name': 'Playwright Sample Test',
+            'user': 'kumarancool20',
+            'accessKey': 'r0hT9zVdrW2RblbW2hUvS5YaBZoGctGymCFyR7JG0KrWfS6Jhq',
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+    const page = await browser.newPage()
     await page.goto("https://www.lambdatest.com/selenium-playground/");
     await page.locator('//a[text()="Drag & Drop Sliders"]').click();
     await page.waitForTimeout(4000)
@@ -21,28 +59,47 @@ test("Second Scenario test", async ({page}) => {
     let percentage = await page.locator('//h4[text()=" Default value 15"]/parent::div//output').textContent();
     await expect(percentage).toContain('95')
 });
-test("Third Scenario test", async ({page}) => {
+test("Third Scenario test", async () => {
+    const capabilities = {
+        'browserName': 'Chrome', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+        'browserVersion': 'latest',
+        'LT:Options': {
+            'platform': 'Windows 10',
+            'build': 'Playwright Sample Build',
+            'name': 'Playwright Sample Test',
+            'user': 'kumarancool20',
+            'accessKey': 'r0hT9zVdrW2RblbW2hUvS5YaBZoGctGymCFyR7JG0KrWfS6Jhq',
+            'network': true,
+            'video': true,
+            'console': true
+        }
+    }
+
+    const browser = await chromium.connect({
+        wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    })
+    const page = await browser.newPage()
     await page.goto("https://www.lambdatest.com/selenium-playground/");
     await page.locator('//a[text()="Input Form Submit"]').click();
     await page.locator('//button[text()="Submit"]').click()
     let name = await page.locator('//input[@id="name"]');
     const validationMessage = await name.evaluate((element) => {
         return element.validationMessage
-      });
-      await expect(validationMessage).toContain('Please fill out this field.')
-      await name.fill('Kumaran')
-      await page.locator('//input[@id="inputEmail4"]').fill('sample@gmail.com')
-      await page.locator('//input[@id="inputPassword4"]').fill('Qwerty');
-      await page.locator('//input[@id="company"]').fill('lamdatest');
-      await page.locator('//input[@id="websitename"]').fill('www.abc.com')
-      await page.locator('//select[@name="country"]').selectOption({label:'United States'})
-      await page.locator('//input[@id="inputCity"]').fill('china');
-      await page.locator('//input[@id="inputAddress1"]').fill('vivekananda street')
-      await page.locator('//input[@id="inputAddress2"]').fill('dubai Kuruku santhu')
-      await page.locator('//input[@id="inputState"]').fill('dubai')
-      await page.locator('//input[@id="inputZip"]').fill('2309986')
-      await page.locator('//button[text()="Submit"]').click()
-      let message=await page.locator('//p[@class="success-msg hidden"]').textContent();
-      await expect(message).toContain('Thanks for contacting us, we will get back to you shortly.')
+    });
+    await expect(validationMessage).toContain('Please fill out this field.')
+    await name.fill('Kumaran')
+    await page.locator('//input[@id="inputEmail4"]').fill('sample@gmail.com')
+    await page.locator('//input[@id="inputPassword4"]').fill('Qwerty');
+    await page.locator('//input[@id="company"]').fill('lamdatest');
+    await page.locator('//input[@id="websitename"]').fill('www.abc.com')
+    await page.locator('//select[@name="country"]').selectOption({ label: 'United States' })
+    await page.locator('//input[@id="inputCity"]').fill('china');
+    await page.locator('//input[@id="inputAddress1"]').fill('vivekananda street')
+    await page.locator('//input[@id="inputAddress2"]').fill('dubai Kuruku santhu')
+    await page.locator('//input[@id="inputState"]').fill('dubai')
+    await page.locator('//input[@id="inputZip"]').fill('2309986')
+    await page.locator('//button[text()="Submit"]').click()
+    let message = await page.locator('//p[@class="success-msg hidden"]').textContent();
+    await expect(message).toContain('Thanks for contacting us, we will get back to you shortly.')
 
 });
